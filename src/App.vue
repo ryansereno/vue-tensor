5f16394ecfb990bb6bbfe6f04040a8840cc80282
ryansereno <ryansereno@protonmail.com>
2023-09-07 17:27:36 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <table border="1">
+  <tr>
+    <td>0.42</td>
+    <td>1.28</td>
+    <td>3.54</td>
+  </tr>
+  <tr>
+    <td>4.67</td>
+    <td>5.82</td>
+    <td>6.19</td>
+  </tr>
+  <tr>
+    <td>7.21</td>
+    <td>8.34</td>
+    <td>9.48</td>
+  </tr>
+</table>
+
+</template>
+
+<style scoped>
+
+
+</style>
