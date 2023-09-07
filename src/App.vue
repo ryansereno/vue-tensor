7eed4ee2c6e355c3d7ce87d912ccd96c0d8e52c9
ryansereno <ryansereno@protonmail.com>
2023-09-07 18:05:40 -0400

    created initial multiplication logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <table border="1">
-  <tr>
-    <td>0.42</td>
-    <td>1.28</td>
-    <td>3.54</td>
-  </tr>
-  <tr>
-    <td>4.67</td>
-    <td>5.82</td>
-    <td>6.19</td>
-  </tr>
-  <tr>
-    <td>7.21</td>
-    <td>8.34</td>
-    <td>9.48</td>
-  </tr>
-</table>
-
+  <div style="display: flex; align-items: center">
+    <button @click="multiplyMatrices">Multiply Matrices</button>
+    <Matrix :matrix="matrixA" />
+    <span> X </span>
+    <Matrix :matrix="matrixB" />
+    <Matrix v-if="resultMatrix" :matrix="resultMatrix" />
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import { ref } from "vue";
+import Matrix from "./components/Matrix.vue"; // adjust path as necessary
 
+const matrixA = ref([
+  [0.42, 1.28, 3.54],
+  [0.42, 1.28, 3.54],
+  [0.42, 1.28, 3.54],
+  [4.67, 5.82, 6.19],
+  [7.21, 8.34, 9.48],
+]);
 
-</style>
+const matrixB = ref([
+  [1.42, 0.28, 2.54],
+  [3.67, 2.82, 1.19],
+  [2.21, 3.34, 4.48],
+]);
+
+const resultMatrix = ref(null);
+
+const multiplyMatrices = () => {
+  const rowsA = matrixA.value.length;
+  const colsA = matrixA.value[0].length;
+  const rowsB = matrixB.value.length;
+  const colsB = matrixB.value[0].length;
+
+  if (colsA !== rowsB) {
+    console.error("Matrix multiplication not possible");
+    return;
+  }
+
+  const result = Array(rowsA)
+    .fill(0)
+    .map((row) => Array(colsB).fill(0));
+
+  for (let i = 0; i < rowsA; i++) {
+    for (let j = 0; j < colsB; j++) {
+      for (let k = 0; k < colsA; k++) {
+        result[i][j] += matrixA.value[i][k] * matrixB.value[k][j];
+      }
+    }
+  }
+
+  resultMatrix.value = result;
+};
+</script>
---
+++ src/components/Matrix.vue
@@ -0,0 +1,16 @@
+<template>
+  <table border="1">
+    <tr v-for="(row, rowIndex) in matrix" :key="rowIndex">
+      <td v-for="(column, colIndex) in row" :key="colIndex">
+        {{ column }}
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { defineProps } from 'vue';
+
+const props = defineProps(['matrix']);
+</script>
