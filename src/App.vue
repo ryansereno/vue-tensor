63c2f672803e8c6d77d07428cc80a6edec9ee61e
ryansereno <ryansereno@protonmail.com>
2023-09-16 12:27:19 -0400

    fixed transformation function

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div style="display: flex">
     <button @click="multiplyMatrices">Multiply Matrices</button>
-    <Matrix :matrix="matrixA" />
+    <Matrix ref="matrixARef" :matrix="matrixA" />
     <span> X </span>
     <div
-      :class="['matrix-transition', { 'matrix-transformed': isTransformed }]"
+      ref="matrixBContainer"
+      :style="transformStyle"
+      class="matrix-transition"
     >
-      <Matrix :matrix="matrixB" />
+      <Matrix ref="matrixBRef" :matrix="matrixB" />
     </div>
-    <Matrix v-if="false" :matrix="resultMatrix" />
   </div>
 </template>
 
@@ -17,18 +18,24 @@ import { ref } from "vue";
 import Matrix from "./components/Matrix.vue"; // adjust path as necessary
 
 const matrixA = ref([
-  [0.42, 1.28, 3.54],
-  [0.42, 1.28, 3.54],
-  [0.42, 1.28, 3.54],
-  [4.67, 5.82, 6.19],
-  [7.21, 8.34, 9.48],
+  [0.42, 1.28, 1.28, 3.54],
+  [0.42, 1.28, 1.28, 3.54],
+  [0.42, 1.28, 1.28, 3.54],
+  [4.67, 5.82, 5.82, 6.19],
+  [7.21, 8.34, 8.34, 9.48],
 ]);
 
 const matrixB = ref([
   [1.42, 0.28, 2.54],
   [3.67, 2.82, 1.19],
   [2.21, 3.34, 4.48],
+  [1.42, 0.28, 2.54],
 ]);
+const transformStyle = ref({});
+
+const matrixARef = ref(null);
+const matrixBRef = ref(null);
+const matrixBContainerRef = ref(null);
 
 const isTransformed = ref(false);
 const resultMatrix = ref(null);
@@ -58,20 +65,38 @@ const multiplyMatrices = () => {
 
   resultMatrix.value = result;
   isTransformed.value = true;
+  setTransformStyle();
+};
+const setTransformStyle = () => {
+  const matrixAEl = matrixARef.value.$el;
+  const matrixBEl = matrixBRef.value.$el;
+
+  const matrixARect = matrixAEl.getBoundingClientRect();
+  const matrixBRect = matrixBEl.getBoundingClientRect();
+
+  // Calculate the necessary transform values
+  const translateX = matrixARect.left - matrixBRect.left;
+  const translateY = matrixARect.top - matrixBRect.top;
+
+  transformStyle.value = {
+    transform: `translateX(${translateX}px) translateY(${translateY}px) rotateZ(-90deg)`,
+    transformOrigin: "top left",
+  };
+
+  isTransformed.value = true;
 };
 </script>
 
 <style>
 .matrix-transition,
-.matrix-transition td{
+.matrix-transition td {
   transition: transform 1s, top 1s, left 1s;
 }
 .matrix-transformed {
-  transform: translateX(90px) translateY(-250px) rotateZ(-90deg);
   transform-origin: left bottom;
 }
-.matrix-transformed td {
+/*.matrix-transformed td {
   transform: rotateZ(90deg);
   transform-origin: center center;
-}
+}*/
 </style>
