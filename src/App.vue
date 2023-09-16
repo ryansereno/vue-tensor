487d997820b6717c2c754efd70a34ee2fa7a692a
ryansereno <ryansereno@protonmail.com>
2023-09-16 12:29:21 -0400

    fixed vertical alignment

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="display: flex">
+  <div style="display: flex; align-items:center;">
     <button @click="multiplyMatrices">Multiply Matrices</button>
     <Matrix ref="matrixARef" :matrix="matrixA" />
     <span> X </span>
